853ca4d7a82ad7d1265a4a1eaa549f381f33ae26
Oknown15 <buchijunior90@gmail.com>
2024-12-22 20:41:08 +0100

    80% completed

--- src/pages/events.vue
+++ src/pages/events.vue
@@ -2,10 +2,10 @@
     <div>
         <Header/>
         <div class="events-container">
-            <img class="events-img" src="../assets/image/events-image.jpg" alt="">
+            <img class="events-img" :src="Events" alt="">
             <p class="page-info">Information concerning our events would be passed on here</p>
-            <div class="events-holder">
-                <button class="upcoming-events-btn">UPCOMING EVENTS</button>
+            <div class="events-holder bg-black">
+                <button disabled class="upcoming-events-btn  d-flex align-items-center justify-content-center">UPCOMING EVENTS</button>
                 <p class="no-events-txt">No Upcoming Events!!!</p>
             </div>
             <div class="more-info">
@@ -14,19 +14,25 @@
         </div>
 
     </div>
-        <Footer/>
+    <Footer/>
 </template>
 
 
 <script>
+    import Events from '../assets/image/events.webp'
     import Header from '../components/header.vue';
     import Footer from '../components/footer.vue';
 export default {
-    name: 'events',
+    data() {
+        return{
+            name: 'events',
+            Events
+        }
+    },
     components: {
-    Header,
-    Footer
-  },
+        Header,
+        Footer
+    },
 }
 </script>
 <style>
